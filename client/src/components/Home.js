ef12e4ad78cbeed2c4e8572241d50723800823e3
Lillian Bu <lbu5@mit.edu>
2019-02-27 18:20:50 -0500

    insert clockface

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,17 +5,36 @@ class Home extends React.Component {
 
   constructor(props){
     super(props);
+
+    this.state = {
+      am: true
+    }
   }
 
   redirectToTarget = () => {
     this.props.history.push('/profile')//sends to home
   }
 
+  toggle = () => {
+    this.setState({
+      am: !this.state.am
+    })
+    console.log(this.state.am)
+  }
+
   render() {
     return (
-      <div>
-        Home
-        <button onClick={this.redirectToTarget}>Profile</button>
+      <div className="center">
+        <div className="clock-container">
+          <button onClick={this.redirectToTarget}>Profile</button>
+          <div>
+            <img className="clockface" src={require('../css/clockface.png')} />
+          </div>
+          <div>
+            <button onClick={this.toggle}>AM</button>
+            <button onClick={this.toggle}>PM</button>
+          </div>
+        </div>
       </div>
     )
     ;
--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,17 @@ module.exports = {
             loader: 'css-loader',
           }
         ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
       }
     ]
   },
